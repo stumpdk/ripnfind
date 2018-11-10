e5188696f1be142713fdcad74d895372573ea957
Bo Henriksen <stumpdenmark@hotmail.com>
2018-11-10 12:38:13 +0100

    Now with top popular names

--- ripnfind.py
+++ ripnfind.py
@@ -1,20 +1,30 @@
 #Recursive Incremental Improving Name Finder
 import difflib
 import json
+from collections import Counter
+import pandas as pd
 
+person = pd.read_csv("hack4dk_police_person.csv")
+# extract all last names
+lastnames_col = person.lastname
+
+# nr. of occurencies of last names
+counter = Counter(lastnames_col)
+counter.most_common(20)
 
 with open('unique_lastnames.json') as f:
     words = json.load(f)
 
 words_lower = []
-for w in words:
-    words_lower.append(w.lower())
+for w in counter.most_common(200):
+    print(w[0])
+    words_lower.append(w[0].lower())
 
 names = words_lower#['wilhelm', 'frederiksen','morkebla']#words_lower
 
 alternativSpellings = {}
 percentileThredshold = 0.9
-cutoffThreshold = 0.7
+cutoffThreshold = 0.75
 def recursiveNearMatcher(sentence, names, alternativSpellings, currentPercent = 0.8, currentCutoff = 0.95):
     matches = list()
     
@@ -22,7 +32,7 @@ def recursiveNearMatcher(sentence, names, alternativSpellings, currentPercent = 
     print("using %d names and %d alternative spellings" % (len(names), len(alternativSpellings)))
     print("sentence %s" % sentence[0:100])
 
-    while len(matches) == 0 and currentPercent < percentileThredshold and currentCutoff > cutoffThreshold:
+    while len(matches) == 0 and currentPercent < percentileThredshold and currentCutoff >= cutoffThreshold:
         for word in sentence.split():
             word = word.lower()
             if len(word)<4:
@@ -51,7 +61,7 @@ def recursiveNearMatcher(sentence, names, alternativSpellings, currentPercent = 
                 break
         
         
-        currentCutoff = currentCutoff-0.1
+        currentCutoff = currentCutoff-0.05
         currentPercent = currentPercent+0.0
         
         #No matches, reduced thresholds
